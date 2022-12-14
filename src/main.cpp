93d13307b936e25d8e04455cbdc8859dd2b9bea1
Brandon <xcobalt10101@gmail.com>
2022-12-13 21:41:10 -0600

    ignore compiled files

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 128);
 int main(int argc, char **argv) {
   TGAImage image(100, 100, TGAImage::RGB);
-  image.set(52, 41, red);
+  image.set(51, 41, red);
   image.flip_vertically();
   image.write_tga_file("output.tga");
   return 0;
